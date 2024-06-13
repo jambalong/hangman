391e6d43ebeb8993ccf8ede8cc583da4aedd25fa
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-06-13 06:57:28 -0700

    Add basic game loop

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,20 @@
 require_relative 'display'
 
 class Game
-  attr_reader :secret_word, :incorrect_guesses, :correct_letters, :incorrect_letters
+  attr_accessor :incorrect_guesses
+  attr_reader :secret_word, :correct_letters, :incorrect_letters
   include Display
 
   def initialize
-    @secret_word = random_secret_word
-    @correct_letters = secret_word.chars.map! { '_' }
+    @secret_word = random_secret_word.chars
+    @correct_letters = secret_word.map { '_' }
     @incorrect_letters = []
     @incorrect_guesses = 0
   end
 
   def play
     update_display
+    new_game
   end
 
   private
@@ -26,5 +28,44 @@ class Game
     display_title
     display_hangman(incorrect_guesses)
     display_letters(correct_letters, incorrect_letters)
+  end
+
+  def valid_guess?(guess)
+    guess.match?(/\p{Alpha}/) && guess.length == 1
+  end
+
+  def new_game
+    loop do
+      puts "Make a guess:"
+      guess = gets.chomp.upcase
+
+      unless valid_guess?(guess)
+        puts "Invalid Input. Please enter a single letter."
+        sleep 1
+        next
+      end
+
+      if secret_word.include?(guess)
+        positions = secret_word.each_index.select { |i| secret_word[i] == guess }
+        correct_letters.map!.with_index { |letter, index| positions.include?(index) ? guess : letter }
+      else
+        incorrect_letters << guess
+        self.incorrect_guesses += 1
+      end
+    
+      update_display
+    
+      break if game_over?
+    end
+    
+    puts game_won? ? "Congratulations!" : "Game Over!"
+  end
+
+  def game_over?
+    incorrect_guesses == 7 || secret_word == correct_letters
+  end
+  
+  def game_won?
+    secret_word == correct_letters
   end
 end
